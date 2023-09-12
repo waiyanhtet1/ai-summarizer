d00d0e2f591967eb41c0f5f0495763b93ea76c92
Wai Yan Htet <waiyanhtet1661390@gmail.com>
2023-09-12 18:04:05 +0630

    feat: add hero section in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "./App.css";
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient"></div>
+      </div>
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
 }
 
 export default App;
